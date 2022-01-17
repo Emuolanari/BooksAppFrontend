64f3992021ad5f83ece4b620c146179226dc5453
Emuolanari Ogulu <emuolanariogulu@Emuolanaris-MacBook-Air.local>
2022-01-17 19:45:08 +0000

    minor

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
 import Menu from "./Menu";
+import Footer from "./Footer";
 
 const About = () => {
     return (
         <div>
             <Menu />
+
+            <Footer />
         </div>
     )
 }
